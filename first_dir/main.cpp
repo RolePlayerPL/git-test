6efbf57319eb6ae46a00a72f8c324709344a13c5
marhub <mariusz.hubczak@fitech.pl>
2021-07-15 13:50:41 +0200

    fizbuzz z wykrz

--- first_dir/main.cpp
+++ first_dir/main.cpp
@@ -8,7 +8,7 @@ int main() {
   // Brain explodes here:
   for (int i = 1; i <= 100; i++){
     if (i % 3 == 0 && i % 5 == 0){
-      finalAnswer = "FizzBuzz";
+      finalAnswer = "FizzBuzz!!!";
       std::cout << finalAnswer << std::endl;
     }
     else if (i % 3 == 0){
